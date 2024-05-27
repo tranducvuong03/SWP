784cfc7fdd3d070a2ee37e2820fcd44ca88a964e
NguyenDangkh <dangkhoa12305053@gmail.com>
2024-05-27 21:22:25 +0700

    .

--- BE/DiamondShop/DiamondShop/obj/Debug/net8.0/DiamondShop.AssemblyInfo.cs
+++ BE/DiamondShop/DiamondShop/obj/Debug/net8.0/DiamondShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf97ad22b316e5243c10d7d389a8e3896995624f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4ed6ecf83413ad31ea95778ef98846509d9e9b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
