87128d0c60a1bcaf419aee700152fe4a5080da3d
tranducvuong03 <tranducvuong31012003@gmail.com>
2024-05-27 20:44:52 +0700

    fix code first

--- BE/DiamondShop/DiamondShop/obj/Debug/net8.0/DiamondShop.AssemblyInfo.cs
+++ BE/DiamondShop/DiamondShop/obj/Debug/net8.0/DiamondShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c60aee8e2d400d615a4f0a422533ec08ce4dd54b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0471b84d5cbdd387416c1d763d11bf26c4887ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
