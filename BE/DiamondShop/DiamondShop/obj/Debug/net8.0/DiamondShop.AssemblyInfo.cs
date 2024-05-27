28def16595ae9d595179e51f0dfa488710bac789
NguyenDangkh <dangkhoa12305053@gmail.com>
2024-05-27 18:46:40 +0700

    smth DB

--- BE/DiamondShop/DiamondShop/obj/Debug/net8.0/DiamondShop.AssemblyInfo.cs
+++ BE/DiamondShop/DiamondShop/obj/Debug/net8.0/DiamondShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d1f422069412e9511f9508d73955e4aec1c812f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf97ad22b316e5243c10d7d389a8e3896995624f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
