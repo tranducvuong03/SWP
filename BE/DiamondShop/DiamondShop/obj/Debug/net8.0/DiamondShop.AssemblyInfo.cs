9b1276484bc9fb24bf2d42c5bf621557ff313236
NguyenDangkh <dangkhoa12305053@gmail.com>
2024-06-01 16:17:25 +0700

    repair user

--- BE/DiamondShop/DiamondShop/obj/Debug/net8.0/DiamondShop.AssemblyInfo.cs
+++ BE/DiamondShop/DiamondShop/obj/Debug/net8.0/DiamondShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiamondShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3462d90536fb717e483d451debcfe111570254c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e46c6ec1f54a88194fee4db91fa98ebdcef9916")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiamondShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiamondShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
