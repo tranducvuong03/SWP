b32065ff33335a87774f3578c48cecf8a0ff2b0b
NguyenDangkh <dangkhoa12305053@gmail.com>
2024-06-01 16:15:11 +0700

    new usercontroller

--- BE/DiamondShop/DiamondShop/Program.cs
+++ BE/DiamondShop/DiamondShop/Program.cs
@@ -4,79 +4,75 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
-using System.Configuration;
-using Microsoft.AspNetCore.Authentication;
-using DiamondShop.Controllers;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("appsettings.json", optional: false);
-var app = builder.Build();
+
 // Add services to the container.
-var jwtSettings = app.Services.GetRequiredService<IOptions<JwtSettings>>().Value;
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-	.AddJwtBearer(options =>
-	{
-		options.TokenValidationParameters = new TokenValidationParameters
-		{
-			ValidateIssuerSigningKey = true,
-			IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
-			.GetBytes(jwtSettings.SecretKey)),
-			ValidateIssuer = false,
-			ValidateAudience = false
-
-		};
-	});
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
-	options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-	{
-		Description = "Standard authorization: (\"{token}\")",
-		In = ParameterLocation.Header,
-		Name = "Authorization",
-		Type = SecuritySchemeType.ApiKey
-	});
-	options.OperationFilter<SecurityRequirementsOperationFilter>();
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Standard Authorization header using the Bearer scheme (\"{token}\")",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT"
+    });
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
 });
 
+// Configure CORS
+builder.Services.AddCors(options => options.AddPolicy("AllowSpecificOrigin", policy =>
+    policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+
+// Configure DbContext
 builder.Services.AddDbContext<DiamondDbContext>(options =>
 {
-	options.UseSqlServer(builder.Configuration.GetConnectionString("DB"));
-	options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DB"));
+    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
 });
 
-builder.Services.AddCors(options =>
+// Configure JwtSettings
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("Jwt"));
+
+// Configure JWT Authentication
+var jwtSettings = builder.Configuration.GetSection("Jwt").Get<JwtSettings>();
+var key = Encoding.ASCII.GetBytes(jwtSettings.SecretKey);
+builder.Services.AddAuthentication(options =>
 {
-	options.AddPolicy("AllowSpecificOrigin",
-	builder =>
-	{
-		builder.WithOrigins("http://localhost:3000")
-	.AllowAnyHeader()
-	.AllowAnyMethod();
-	});
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings.Issuer,
+        ValidAudience = jwtSettings.Audience,
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
 });
-//JWT SETTING
 
-// up JWT
-//var jwtSettings = new JwtSettings();
-builder.Configuration.Bind("Jwt", jwtSettings);
-
-
-// Configure Authentication
-
+// Build the app
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseCors("AllowSpecificOrigin");
