3910b4a8b662089aea5fbb348b0a826cbf905172
tranducvuong03 <tranducvuong31012003@gmail.com>
2024-06-04 12:11:30 +0700

    gitignore debug folder

---
+++ BE/DiamondShop/DiamondShop/Model/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DiamondShop.Model
+{
+	public class UserModel
+	{
+		public string UserId { get; set; }
+		public int RoleId { get; set; }
+		public int CartId { get; set; }
+		public string? Email { get; set; }
+		public string? Status { get; set; }
+		public string? FullName { get; set; }
+		public string Username { get; set; }
+		public string Password { get; set; }
+		public bool IsActive { get; set; }
+	}
+}
